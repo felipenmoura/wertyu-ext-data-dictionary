3e5a7c13830fd2f35d8e9f34a8ce74944699c421
Felipe Nascimento de Moura <felipenmoura@gmail.com>
2020-06-11 15:37:55 -0300

    First commit

---
+++ index.js
@@ -0,0 +1,17 @@
+module.exports = {
+  meta: {
+    permissions: ["settings", "global-settings"],
+    wertyuVersion: 1,
+  },
+  sideBar: {
+    name: "Data Dictionary",
+    icon: './sidebar-icon.png',
+    content: './sidebar-panel.html'
+  },
+  panel: {
+    name: "Data Dictionary",
+    description: "Your entitie's structure is described in a Data Dictionary",
+    icon: './sidebar-icon.png',
+    content: './panel.html'
+  }
+};
